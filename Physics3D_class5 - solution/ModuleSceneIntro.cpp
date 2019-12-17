bb7bb9524be894727a29093d9aecd6d2afd784f7
Bernat Casanas <bernatcasanas@gmail.com>
2019-12-17 11:38:39 +0100

    obstacles done

--- Physics3D_class5 - solution/ModuleSceneIntro.cpp
+++ Physics3D_class5 - solution/ModuleSceneIntro.cpp
@@ -4,6 +4,7 @@
 #include "Primitive.h"
 #include "PhysBody3D.h"
 #include "PhysVehicle3D.h"
+#include "time.h"
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -17,7 +18,10 @@ bool ModuleSceneIntro::Start()
 {
 	LOG("Loading Intro assets");
 	bool ret = true;
+	srand(time(NULL));
+	float r;
 
+	//Horizontal Limits
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
 	float z = 0, x = -7;
@@ -25,9 +29,17 @@ bool ModuleSceneIntro::Start()
 		createRectangle({ 8, 0, z }, { 1, 2, 1 });
 		createRectangle({ -8, 0, z }, { 1, 2, 1 });
 	}
-	for (x; x < 8; x += 1.5) createRectangle({ x, 0, 0 }, { 1, 2, 1 });
+
+	//Top Bottom Limits
+	for (x; x < 8; x += 1.5) createRectangle({ x, 0, 0 }, { 1, 2, 1 }); 
 	x = -7;
 	for (x; x < 8; x += 1.5) createRectangle({ x, 0, z }, { 1, 2, 1 });
+
+	//Obstacles Randomly setted
+	for (float i = 6; i <= 500; i += 9) {
+		r = rand() % 14 - 7;
+		createRectangle({ r, 0, i }, { 1, 2, 1 });
+	}
 
 	return ret;
 }
